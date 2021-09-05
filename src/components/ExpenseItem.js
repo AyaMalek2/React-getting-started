336786fb9cfbf4a1348d3d51f55968a8029769f2
Aya Malek <aya.malek142@gmail.com>
2021-09-05 16:54:13 +0200

    reusable expenses using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 //return statement can only have one root element
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   //   return <h2>ExpenseItem works!</h2>;
   const expenseDate = new Date(2021, 8, 5); //Sep 5 2021
   const expenseTitle = "car insurance";
   const expensePrice = 290;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      <h2 className="expense-item__description">{expenseTitle}</h2>
-      <div className="expense-item__price">${expensePrice}</div>
+      <div>{props.date.toISOString()}</div>
+      <h2 className="expense-item__description">{props.title}</h2>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   );
 }
