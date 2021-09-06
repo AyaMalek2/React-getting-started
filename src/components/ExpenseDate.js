6de7fdf37bc992505cbd34ea8b40d60aa19ef82b
Aya Malek <aya.malek142@gmail.com>
2021-09-06 11:50:54 +0200

    add expenses list to expenses component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import "./ExpenseDate.css";
+
+export default function ExpenseDate(props) {
+  console.log("date", props);
+  //props is an object(key: attribute spicified in the jsx in the parent, value )
+  const month = props.date.toLocaleDateString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleDateString("en-US", { day: "2-digit" });
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,16 @@
 //return statement can only have one root element
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 export default function ExpenseItem(props) {
   //   return <h2>ExpenseItem works!</h2>;
   const expenseDate = new Date(2021, 8, 5).toLocaleDateString(); //Sep 5 2021
   const expenseTitle = "car insurance";
   const expensePrice = 290;
-
-  const month = props.date.toLocaleDateString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleDateString("en-US", { day: "2-digit" });
-
+  console.log("item: ", props);
   return (
     <div className="expense-item">
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+      <ExpenseDate date={props.date} />
       <h2 className="expense-item__description">{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
     </div>
