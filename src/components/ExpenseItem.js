f6728b1c1a1ddd9bc82fa2c9c88ee304fb215f2d
Aya Malek <aya.malek142@gmail.com>
2021-09-06 11:08:35 +0200

    format the date in expenses componet

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,12 +3,19 @@ import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
   //   return <h2>ExpenseItem works!</h2>;
-  const expenseDate = new Date(2021, 8, 5); //Sep 5 2021
+  const expenseDate = new Date(2021, 8, 5).toLocaleDateString(); //Sep 5 2021
   const expenseTitle = "car insurance";
   const expensePrice = 290;
+
+  const month = props.date.toLocaleDateString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleDateString("en-US", { day: "2-digit" });
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
       <h2 className="expense-item__description">{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
     </div>
